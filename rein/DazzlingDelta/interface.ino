194dc99b939f65cb33299cb63620c6b6e8e4c420
ReinAquit <aquit4life@gmail.com>
2019-06-12 14:48:54 +0200

    File change + interface

---
+++ rein/DazzlingDelta/interface.ino
@@ -0,0 +1,22 @@
+
+
+
+void setup()
+{
+  Serial.begin(9600);
+
+  
+}
+
+void loop()
+{
+  if (Serial.available() > 0) {
+                // read the incoming byte:
+                incomingByte = Serial.read();
+
+                // say what you got:
+                Serial.print("I received: ");
+                Serial.println(incomingByte, DEC);
+
+  
+}
